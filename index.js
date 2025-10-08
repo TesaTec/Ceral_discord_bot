eb8ed2d0be589d463d9ed170fed91e7e21189096
TesaTec <144019532+TesaTec@users.noreply.github.com>
2025-10-08 15:33:37 +0200

    Finished the loading of the command files

--- index.js
+++ index.js
@@ -20,4 +20,13 @@ const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js')); 
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, comand);
+        } else {
+            console.log((`[Warning] The command at ${filePath} us nussubg a required "data" or "execute" property.` ));
+        }
+    }
 }
